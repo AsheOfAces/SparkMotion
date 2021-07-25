0faa48a29a8ded6bbc45613a2090a07760a18d87
Acey <theaidency@gmail.com>
2021-07-25 11:44:19 +0530

    Smoother movement

--- Assets/Scripts/PinWheel.cs
+++ Assets/Scripts/PinWheel.cs
@@ -29,6 +29,8 @@ public class PinWheel : MonoBehaviour
     Vector3 distanceVector;
     float spokeLength;
     public float vDistance, vRatio, normalisedFactor;
+    Quaternion targetRotation;
+    bool surveyorPassed = false;
     
     // Start is called before the first frame update
     void Start()
@@ -38,12 +40,14 @@ public class PinWheel : MonoBehaviour
         turnCrankSpeed = aMan.turnCrankSpeed;
         distanceVector = new Vector3(0, -1, 0);
         spokeLength = aMan.pinWheelRadius;
+        targetRotation = Quaternion.identity;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.localRotation *= Quaternion.AngleAxis(parentBody.velocity.magnitude * turnCrankSpeed * Time.deltaTime, Vector3.right);
+        targetRotation = targetRotation * Quaternion.AngleAxis(parentBody.velocity.magnitude * turnCrankSpeed * Time.deltaTime, Vector3.right);
+        transform.localRotation = Quaternion.RotateTowards(transform.localRotation, targetRotation, parentBody.velocity.magnitude * turnCrankSpeed * Time.deltaTime);
         Debug.DrawLine(transform.position, transform.position + (transform.TransformDirection(wheelSpokes[0].spokeDirection) * spokeLength), Color.white);
         Debug.DrawLine(transform.position, transform.position + (transform.TransformDirection(wheelSpokes[1].spokeDirection) * spokeLength), Color.white);
         Debug.DrawLine(transform.position, transform.position + (transform.TransformDirection(wheelSpokes[2].spokeDirection) * spokeLength), Color.white);
@@ -52,17 +56,35 @@ public class PinWheel : MonoBehaviour
         Debug.DrawLine(transform.position, transform.position + Vector3.up * spokeLength * -1, Color.cyan);
         vDistance = Vector3.Distance(transform.position + Vector3.up * spokeLength * -1, transform.position + (transform.TransformDirection(wheelSpokes[activeSpoke].spokeDirection) * spokeLength));
         vRatio = Mathf.Clamp01(vDistance/1.4f);
-        if(vRatio<0.3)
+        if(vDistance <= 0.3)
         {
-            vRatio = 0.3f;
-            
+            surveyorPassed = true;
         }
-        normalisedFactor = (vRatio - 0.3f) / 0.7f;
+        normalisedFactor = Mathf.Clamp01((vRatio - 0.3f) / 0.7f);
+
+        if (normalisedFactor == 0)
+        {
+            if(surveyorPassed)
+            {
+                if (activeSpoke == 3)
+                {
+                    activeSpoke = 0;
+                }
+                else activeSpoke++;
+                surveyorPassed = false;
+            }
+        }
+        
 
         
 
 
         aMan.FootAlignment(1- (normalisedFactor), wheelSpokes[activeSpoke].footAlignment.passReach, wheelSpokes[activeSpoke].footAlignment.leftRight);
 
+    }
+
+    private void LateUpdate()
+    {
+        
     }
 }
